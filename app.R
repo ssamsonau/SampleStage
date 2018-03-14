0b15b441188a2d632b6cba7e9cd49bfb52597314
Sergey Samsonau <s.samsonau@gmail.com>
2018-03-14 13:40:50 -0400

    small correction

--- app.R
+++ app.R
@@ -8,7 +8,6 @@
 #
 
 library(shiny)
-library(DT)
 
 
 #https://stackoverflow.com/questions/30587883/is-it-possible-to-stop-executing-of-r-code-inside-shiny-without-stopping-the-sh
@@ -28,7 +27,7 @@ ui <- fluidPage(
         h4("-----"),
         
         numericInput("speed", 
-                     "Set up speed of stage motion (mm/sec)",  # program takes mm/min, so it should be later * 60
+                     "How fast should the stage move (mm/sec)",  # program takes mm/min, so it should be later * 60
                      value = 0.02),
         
         ## USE inkscape svg - to path to specify coordinates for continous motion with a selected speed
@@ -37,74 +36,48 @@ ui <- fluidPage(
         
         # 
         textInput("move_by_xy",
-                     "Initial poisitioning: Move sample by so many mm: X, Y",
+                     "Move sample by so many mm: X, Y",
                      value = "-0.1, 0.1"),
         actionButton("move_now_but", "Move now"), # not implement
         actionButton("set_current_asX0Y0", "Set current position as X0Y0"), 
-        actionButton("return_toX0Y0", "Return to X0Y0")
+        actionButton("return_toX0Y0", "Return to X0Y0"), 
+        
+        h4("-----"),
+        numericInput("distance_between_dots",
+                     "Specify distance between dots (both x and y) in mm",
+                     value = 0.1),
+        numericInput("xyRange",
+                     "specify range of mapping (range of total x and y movement) in mm",
+                     value = 0.5),
+
+        # numericInput("time_takes_to_move",
+        #              "Time it takes to move between dots (in sec)",
+        #              value = 2),
+        # numericInput("time_to_stay_in_each_dot",
+        #              "Time to stay in each dot (in sec)",
+        #              value = 10),
+        numericInput("spectrum_collection_time", 
+                  "Time setup in Andor Solis for specrum collection (in sec)", 
+                  value = 5),
+        
+        textInput("coordinates_of_takeSignal_button_for_mouse",
+                  "Specify coordinates of 'Take signal' button on Andor Solis",
+                  value = "158, 70"),
+        # textInput("coordinates_of_save_button_for_mouse",
+        #           "Specify coordinates of 'Save' button on Andor Solis",
+        #           value = "84, 70"),
+        
+       # actionButton("stop", "Stop"),
+        
+        h4("(will move in dot by dot pattern,
+                     starting with present position. First go up, then right, then donw, then right, then up, etc."),
+        actionButton("do_mapping_but", "Execute mapping"),
+        actionButton("stop_mapping_but", "Interupt (stop) mapping (DOES NOT WORK...")
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-        
-        tabsetPanel(type = "tabs",
-                    tabPanel("Mapping", 
-                             numericInput("distance_between_dots",
-                                          "Specify distance between dots (both x and y) in mm",
-                                          value = 0.1),
-                             numericInput("xyRange",
-                                          "specify range of mapping (range of total x and y movement) in mm",
-                                          value = 0.5),
-                             
-                             # numericInput("time_takes_to_move",
-                             #              "Time it takes to move between dots (in sec)",
-                             #              value = 2),
-                             # numericInput("time_to_stay_in_each_dot",
-                             #              "Time to stay in each dot (in sec)",
-                             #              value = 10),
-                             numericInput("spectrum_collection_time", 
-                                          "Time setup in Andor Solis for specrum collection (in sec)", 
-                                          value = 5),
-                             
-                             textInput("coordinates_of_takeSignal_button_for_mouse",
-                                       "Specify coordinates of 'Take signal' button on Andor Solis",
-                                       value = "158, 70"),
-                             # textInput("coordinates_of_save_button_for_mouse",
-                             #           "Specify coordinates of 'Save' button on Andor Solis",
-                             #           value = "84, 70"),
-                             
-                             # actionButton("stop", "Stop"),
-                             
-                             helpText("(will move in dot by dot pattern,
-                                starting with present position. First go up, then right, then donw, then right, then up, etc."),
-                             actionButton("do_mapping_but", "Execute mapping"),
-                             actionButton("stop_mapping_but", "Interupt (stop) mapping (DOES NOT WORK..."),
-                             
-                             plotOutput("plot_dots")
-                    ),
-                    tabPanel("Lasing :)", 
-                      fileInput("file1", "Choose Image file",
-                                accept = c(".png", ".jpg", ".jpeg", ".PNG", ".JPG", ".JPEG")
-                      ),
-                      plotOutput("image"),
-                      
-                      numericInput("desired_x_range", 
-                                   "desired x range of values in Image (in mm)", 
-                                   value = 1),
-                      
-                      numericInput("resolution_size", 
-                                   "Round coordinates to this size (in mm)", 
-                                   value = 0.01),
-                      
-                      numericInput("time_to_stay_in_dot",
-                                   "Time to stay in each dot (in sec)",
-                                   value = 1),
-                      
-                      actionButton("move_by_picture_but", "Move according to image file"),
-                      tags$hr(),
-                      DT::dataTableOutput("image_table")
-                    )
-        )
+         plotOutput("plot_dots")
       )
    )
 )
@@ -117,7 +90,6 @@ server <- function(input, output) {
   library(tidyverse)
   library(stringr)
   library(rMouse)
-  library(imager)
   # library(rJava)                     # load package
   # .jinit()                           # this starts the JVM
   # jRobot <- .jnew("java/awt/Robot")  # Create object of the Robot class
@@ -127,7 +99,6 @@ server <- function(input, output) {
    
   source("move_by_xy.R", local = T) 
   source("mapping.R", local = T)
-  source("coordinates_from_image.R", local  = T)
 }
 
 # Run the application 
